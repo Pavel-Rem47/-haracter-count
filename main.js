ad4d89cd3e497571a50fdcec9c28dc1e125cf66f
psremnev <52470169+psremnev@users.noreply.github.com>
2020-10-30 23:45:08 +0300

    +

--- main.js
+++ main.js
@@ -1,38 +1,64 @@
-'use strict';
-function onLoad() {
-
+class WordCount extends HTMLElement {
 	
-	let symb_input = $('.symbol')[0];
-	let whitespace_input = $('.whitespace')[0];
-	let word_input = $('.word')[0];
-	
-	
-	$('.button')[0].onclick = function() {
-		let str = $('.text')[0].value;
-		let whitespace = 0;
-		let word = 0;
-		let symb = 0;
-		
-		str = Array.from(str);
-		
-		if (str.length==0){
-			symb = 0;
-			whitespace = 0;
-		    word = 0;
+	constructor() {
+		super();
+		this.className = 'word-count'
+		this.innerHTML = `
+			<textarea class = 'text-area' type = 'text' autocomplete = 'off' rows="число" placeholder = 'Введите строку...'></textarea>
+				<div class= 'info'>
+					<div><div>Кол-во символов:</div><div class = 'symbol-in-textarea'></div></div>
+					<div><div>Кол-во пробелов:</div><div class = 'space-in-textarea'></div></div>
+					<div><div>Кол-во слов:</div><div class = 'word-in-textarea'></div></div>
+				</div>
+			<input type = 'button' class = 'calculateBtn' value = 'Подсчитать'>
+		`
+		this.textArea = this.querySelector('.text-area');
+		this.symbolInTextarea = this.querySelector('.symbol-in-textarea');
+		this.spaceInTextarea = this.querySelector('.space-in-textarea');
+		this.wordInTextarea = this.querySelector('.word-in-textarea');
+		this.calculateBtn = this.querySelector('.calculateBtn');
+	}
+	connectedCallback() {
+		this.calculate()
+	}
+	calculate() {
+		let area = this.textArea;
+		let symbolInTextarea = this.symbolInTextarea;
+		let spaceInTextarea = this.spaceInTextarea;
+		let wordInTextarea = this.wordInTextarea;
+		this.calculateBtn.onclick = function() {
+			let str = area.value;
+			let space = 0;
+			let word = 0;
+			let symb = 0;
+			
+			str = Array.from(str);
+			
+			if (str.length==0){
+				symb = 0;
+				space = 0;
+				word = 0;
+			}
+			else if (str.length!=0){
+				symb = str.length;
+				word = 1;
+			}
+			for (let i=0; i<str.length-1; i++) {
+					if (str.length!=0 && str[i]==' ') {
+						word++;
+						space++;
+					}
+					
+			}
+			symbolInTextarea.innerHTML = symb;
+			wordInTextarea.innerHTML = word;
+			spaceInTextarea.innerHTML = space;
 		}
-		else if (str.length!=0){
-			symb = str.length;
-			word = 1;
-		}
-		for (let i=0; i<str.length-1; i++) {
-				if (str.length!=0 && str[i]==' ') {
-					word++;
-					whitespace++;
-				}
-				
-		}
-		symb_input.innerHTML = symb;
-		word_input.innerHTML = word;
-		whitespace_input.innerHTML = whitespace;
 	}
 }
+
+customElements.define('word-count', WordCount)
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.body.appendChild(new WordCount())
+});
